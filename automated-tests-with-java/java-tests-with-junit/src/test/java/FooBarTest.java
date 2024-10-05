da32255aab51532be4507200a0c28cfdbeea2d94
Gustavo <gustavo_linhaus@hotmail.com>
2024-10-05 07:57:56 -0300

    Getting Started with JUnit 5 and Java

---
+++ automated-tests-with-java/java-tests-with-junit/src/test/java/FooBarTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+class FooBarTest {
+
+    @Test
+    void test() {
+        // fail("Not yet implemented");
+        System.out.println("Hello JUnit 5!");
+    }
+
+}
+
